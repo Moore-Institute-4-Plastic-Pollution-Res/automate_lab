e3b5ec92429c4d5d88b2684c0b7cc9e9712f48bd
Win Cowger, PhD <wincowger@gmail.com>
2024-11-22 09:16:17 -0800

    update indiv

--- sfei.R
+++ sfei.R
@@ -17,7 +17,7 @@ library(readr)
 project_name <- readline(prompt = "Enter the Name of the Google Drive Folder for this Project: ")
 
 # Load library for analysis
-lib <- read_any("Library/derivative.rds")
+lib <- load_lib("derivative")
 
 lib <- filter_spec(lib, lib$metadata$spectrum_type == "ftir")
 
@@ -27,57 +27,40 @@ data_search <- shared_drive_find("Project") |>
   drive_ls() |> 
   filter(name %in% project_name) |> 
   drive_ls() |> 
-  filter(name == "SFEI_01") |> 
-  drive_ls() |> 
   filter(name == "Particle_500um_Data") |> 
   drive_ls() |> 
   filter(name == "iS50 Spectra") |> 
   drive_ls() |> 
-  filter(name == "SFEI_01_Spectra_Particles") |> 
-  drive_ls() |> 
-  filter(grepl("SFEI", name))
+  filter(name == "Spectra") |> 
+  drive_ls()
   
 # Found the correct folder path - now create a function that conducts the analysis for each folder that starts with SFEI
 
+#Set a folder to save data locally
+folder_placement <- file.path(project_name, "Raw", fsep = "/")
+
+# Create folders ----
+if(!dir.exists(file.path(project_name))){
+  dir.create(file.path(project_name))
+  dir.create(file.path(project_name, "Raw", fsep = "/"))
+  dir.create(file.path(project_name, "Results", fsep = "/"))
+}
+
 # Download files
-# # Download data
-for (folder in 1:nrow(data_search)) {
-  # folders of interest 
-  folder_name <- data_search[folder,]
-  
-  folders_sfei <- folder_name |> 
-    drive_ls() |> 
-    filter(grepl("spa", name)) 
-  
-  folder_placement <- file.path(project_name, "Raw", folder_name$name, fsep = "/")
-  
-  # Create folder paths
-  # Create folders ----
-  if(!dir.exists(file.path(project_name))){
-    dir.create(file.path(project_name))
-  }
-  if(!dir.exists(file.path(folder_placement))){
-    dir.create(file.path(folder_placement), recursive = T)
-  }
-  if(!dir.exists(file.path(project_name,"Results"))){
-    dir.create(file.path(project_name,"Results"))
-  }
-  
-    for (file in 1:nrow(folders_sfei)) {
+    for (file in 1:nrow(data_search)) {
       # files in each specific folder
-      file_name <- folders_sfei$name[file]
+      file_named <- data_search$name[file]
       
-      dest_path <- file.path(folder_placement,file_name, fsep = "/")
+      dest_path <- file.path(folder_placement,file_named, fsep = "/")
       tryCatch({
-        drive_download(as_id(folders_sfei[file, ]$id), path = dest_path,overwrite = TRUE)
+        drive_download(as_id(data_search[file, ]$id), path = dest_path,overwrite = TRUE)
       }, error = function(e) {
         message("Error downloading file: ", file_name, "\n", e)
-      })
-
+      }) #Add a check at the end that 
     }
-  
-    # for each folder conduct the analysis
-    individual_spec <- list.files(folder_placement, full.names = T)
+
+    # for each folder conduct the analysis"
+    individual_spec <- list.files(file.path(project_name, "Raw", fsep = "/"), full.names = T, recursive = T)
     
     indiv_files <- read_any(individual_spec) |>
       c_spec("common") |>
@@ -96,52 +79,66 @@ for (folder in 1:nrow(data_search)) {
     
     top_matches <- match_spec(x = indiv_files, 
                               library = lib, 
-                              top_n = 1, 
+                              top_n = 1,
                               add_library_metadata = "sample_name",
                               add_object_metadata = "col_id") %>%
-      select(file_name.y, match_val, material_class, spectrum_identity, library_id)
+      rename(file_name = file_name.y) |> 
+      mutate(sample = sub(".*_(S\\d{2})_.*", "\\1", file_name)) |> 
+      select(sample,file_name,match_val,material_class,spectrum_identity,library_id)
+    
     
     write.csv(
       top_matches,
       file = file.path(
-        "SFEI",
+        project_name,
         "Results",
-        paste0(folder_name$name,"_individual_particle_matches.csv")
+        "full_particle_results.csv"
       ),
       row.names = F
     )
-}
 
-
-# Results join all the tables together and filter for plastic 
-df <- list.files(path = "SFEI/Results/", full.names = T) |> 
-  lapply(read_csv) |> 
-  bind_rows()
-
-df <- df |> 
-  rename(file_name = file_name.y) |> 
-  mutate(sample = sub(".*_(S\\d{2})_.*", "\\1", file_name)) |> 
-  select(sample,file_name,match_val,material_class,spectrum_identity,library_id)
-
-write_csv(df, "full_particle_results.csv")
-
-just_plastics <- df |> 
-  filter(material_class != "mineral",
+just_plastics <- top_matches |> 
+  dplyr::filter(material_class != "mineral",
          !grepl("organic", material_class))
 
-write.csv(just_plastics, "plastic_df.csv")
+write.csv(
+  just_plastics,
+  file = file.path(
+    project_name,
+    "Results",
+    "plastic_df.csv"
+  ),
+  row.names = F
+)
 
-
- # Upload results
+# Upload results
 # Create new folder
+
+folder_location <- shared_drive_find("Project") |> 
+  drive_ls("Customer Projects") |> 
+  drive_ls() |> 
+  filter(name %in% project_name) |> 
+  drive_ls() |> 
+  filter(name == "Particle_500um_Data") |> 
+  drive_ls() |> 
+  filter(name == "iS50 Spectra") |> 
+  drive_ls() |> 
+  filter(name == "Spectra")
+
 new_folder <- drive_mkdir("Single_Particle_Results",
-                          path = as_id("1_Vsn_bHxkQvT5FEXPo6Aq3ug9yheZdIe"),
+                          path = as_id(folder_location$id),
                           overwrite = TRUE
 )
 
+drive_upload(media = file.path(
+  project_name,
+  "Results",
+  "plastic_df.csv"
+), path = as_id(as.character(new_folder$id)))
 
 
-
-drive_upload(media = "plastic_df.csv", path = as_id(as.character(new_folder$id)))
-
-
+drive_upload(media = file.path(
+  project_name,
+  "Results",
+  "full_particle_results.csv"
+), path = as_id(as.character(new_folder$id)))
