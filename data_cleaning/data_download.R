9db79e49fec31406752bcc44c5d80d1f8d7b8d45
hazelvaq <hvaquero98@gmail.com>
2024-12-18 21:53:23 -0800

    download more data needed

--- data_cleaning/data_download.R
+++ data_cleaning/data_download.R
@@ -3,23 +3,30 @@ library(tidyverse)
 # download particle count locally
 particle_count_data <- folder_find |> 
   filter(grepl("ParticleCount", name)) |> 
-  # asummes particle count on the firt sheet of the spreadsheet
+  # assumes particle count on the first sheet of the spreadsheet
   drive_download(path = file.path(local_store, "particle_count.csv"), overwrite = TRUE, type = "csv")
 
 # fiber
 fiber_count_data <- folder_find |> 
   filter(grepl("Fiber_data", name)) |> 
-  # asummes particle count on the firt sheet of the spreadsheet
+  # assumes particle count on the first sheet of the spreadsheet
   drive_download(path = file.path(local_store, "fiber_data"), overwrite = TRUE, type = "xlsx")
 
 # download Multiplier
 multiplier_data <- folder_find |> 
   filter(grepl("Multiplier", name)) |> 
   # asummes particle count on the firt sheet of the spreadsheet
-  drive_download(path = file.path(local_store, "multiplier"), overwrite = TRUE, type = "xlsx")
+  drive_download(path = file.path(local_store, "Multiplier"), overwrite = TRUE, type = "xlsx")
 
 # lab guru sheet
 lab_guru <- folder_find |> 
   filter(grepl("LabGuru", name)) |> 
-  drive_download(path = file.path(local_store, "lab_guru"), overwrite = TRUE, 
+  drive_download(path = file.path(local_store, "LabGuruSampleWorksheet"), overwrite = TRUE, 
                  type = "xlsx")
+
+# LFB QA/QC
+lfb_counts <- folder_find |> 
+  filter(grepl("LFB_Counts", name)) |> 
+  drive_download(path = file.path(local_store, "LFB_Counts"), overwrite = TRUE, 
+                 type = "csv")
+
